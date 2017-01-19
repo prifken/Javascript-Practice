9ddc97c8ca4f00ef815c16d223620b63edf30f44
Peter Rifken - Work PC <peter.rifken@gmail.com>
2017-01-19 14:12:53 -0500

    milestone
    -Dates on projects tab work
    -Need to fix timeline

--- DHTMLXGANTT/testdata.js
+++ DHTMLXGANTT/testdata.js
@@ -103,7 +103,7 @@ data: request,
                         var x = new projects("string","string"); 
                         x.projRID=$(this).find("record_id_").text();
                         x.projName=$(this).find("project_name").text();
-                        x.projStart=$(this).find("est_start_date"); 
+                        x.projStart=$(this).find("est_start_date").text(); 
                         projArray.push(x);
                         //console.log(coachArray);
                         //console.log(x); 
