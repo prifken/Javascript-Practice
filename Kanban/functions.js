a34749a285dd94017fa0c1fb2eb4a940b7171918
Peter <prifken@users.noreply.github.com>
2018-01-02 08:13:06 -0500

    Milestone
    added ability to assign task name to a person along with a status.

--- Kanban/functions.js
+++ Kanban/functions.js
@@ -71,3 +71,42 @@ console.log("studentid= ",studentid," taskid= ",taskid);
   return rid;   
   
 }
+
+function addnewtask(text,personId,status){
+console.log("text= ",text," personId= ",personId," status= ",status); 
+
+      var url = "https://team.quickbase.com/db/bk7sy9jpi?a=API_AddRecord&usertoken=b287yg_uyp_dsagzw8b755gx7bvr8hfmdp3fu53"
+      url+= "&_fid_141="+personId; //Related Team Member = FID 141
+      url+= "&_fid_6="+text; //Task Name = FID 6
+      url+= "&_fid_159=37" // Set related build season task to 0 - Misc
+      url+= "&_fid_12="+status; //Activtion Status = FID 12, replace with NEW Status
+
+  
+  var rid = 0; 
+    $.ajax({
+          type: "POST",
+          contentType: "text/xml",
+          async: false,
+          url: url,
+          dataType: "xml",
+          processData: false,
+          success: function (response) {
+          //alert("Boom.... success baby");
+              var xml = $(response);
+              if (xml.find('qdbapi').find('errcode').text() === "0") {
+                  rid = xml.find('qdbapi').find('rid').text();
+                  
+              }
+              else {
+                  console.log("Quickbase returned an error.");
+                  console.log(response);
+              } 
+          },
+          error: function (e) {
+               console.log(e);  
+          }
+      }); 
+    console.log("rid = "+rid);
+  return rid;   
+ 
+}
